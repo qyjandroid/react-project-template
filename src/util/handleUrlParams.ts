c7a8839dc8fe5446735da8e152af688ea8b3a02a
quanyj <qyjandroid@163.com>
2021-08-10 13:57:23 +0800

    更新工具类

---
+++ src/util/handleUrlParams.ts
@@ -0,0 +1,48 @@
+/**
+ * 获取 URL 参数
+ * @param name
+ */
+export function getUrlParam(name: string) {
+    // 构造一个含有目标参数的正则表达式对象
+    const reg = new RegExp('(^|&)' + name.toLowerCase() + '=([^&]*)(&|$)');
+    // 匹配目标参数
+    const r = window.location.search
+        .substr(1)
+        .toLowerCase()
+        .match(reg);
+    if (r != null) {
+        //返回参数值
+        return unescape(r[2]);
+    }
+    return null;
+}
+/**
+ * url添加参数
+ * @param {string} url - 需要添加参数的url
+ * @param {object} params - 添加的参数，参数是'key:value'形式
+ * @param {boolean} [encode=false] - 返回的url是否需要编码
+ * @returns {string}
+ */
+export function addParams({ url = '', params = {}, encode = false }: { url?: string; params: object; encode?: boolean }) {
+    if (!Object.keys(params).length) {
+        return url;
+    }
+    url = decodeURIComponent(url);
+    const [hostStr, searchStr] = url.split('?');
+    if (url.includes('?')) {
+        const oldParams = {};
+        searchStr.split('&').forEach(val => {
+            const newVal = val.split('=');
+            oldParams[newVal[0]] = newVal[1];
+        });
+        // 合并、去重参数
+        params = { ...oldParams, ...params };
+    }
+    let [paramsStr, i] = ['', 1];
+    for (const [key, val] of Object.entries(params)) {
+        paramsStr += i > 1 ? `&${key}=${val}` : `${key}=${val}`;
+        i++;
+    }
+    const baseUrl = `${hostStr}?${paramsStr}`;
+    return encode ? encodeURIComponent(baseUrl) : baseUrl;
+}
