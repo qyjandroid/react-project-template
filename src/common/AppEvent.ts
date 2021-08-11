41a9587b3b4dfdfaea04ff2e9b8633c55f85b706
quanyj <qyjandroid@163.com>
2021-08-11 11:35:33 +0800

    增加app事件中心

---
+++ src/common/AppEvent.ts
@@ -0,0 +1,41 @@
+
+
+import { EventEmitter } from "events";
+
+const PREFIX = "app_";
+
+export function getAppEventType(type: string) {
+    return `${PREFIX}${type}`;
+}
+
+/**
+ *
+ * app全局事件触发
+ * @class AppEventEmitterUtils
+ */
+class AppEventEmitterUtils {
+    private emitter: EventEmitter;
+
+    constructor() {
+        this.emitter = new EventEmitter();
+    }
+
+    off = (type, cb) => this.emitter.off(getAppEventType(type), cb);
+
+    emit = (type, data?) => this.emitter.emit(getAppEventType(type), data);
+
+    on = (type, cb) => this.emitter.on(getAppEventType(type), cb);
+
+    destroy() {
+        if (this.emitter) {
+            // 移除全部监听
+            this.emitter.removeAllListeners();
+        }
+    }
+}
+const appEventEmitter = new AppEventEmitterUtils();
+export default appEventEmitter;
+
+export enum AppEventType {
+    InvalidToken = "invalid-token",
+}
