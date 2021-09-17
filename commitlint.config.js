5a7c8da30c3f44afc7e2fe35ce7be4e0d4ad6f40
权跃杰 <quanyj@mail.jj.cn>
2021-09-17 16:39:45 +0800

    docs: 更新commit文档规则

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,20 @@
 // git commit 规范
 // <类型>[可选的作用域]: <描述>
-
+//git commit -m 'feat: 增加 xxx 功能'
+//git commit -m 'bug: 修复 xxx 功能'
 // # 主要type
 // feat:     增加新功能
 // fix:      修复bug
-
-// # 特殊type
-// docs:     只改动了文档相关的内容
+//build:     主要目的是修改项目构建系统(例如 glup，webpack，rollup 的配置等)的提交
+//ci:         主要目的是修改项目继续集成流程(例如 Travis，Jenkins，GitLab CI，Circle等)的提交
+//docs:       文档更新
+//perf:      性能，体验优化
+//refactor:  代码重构时使用
 // style:    不影响代码含义的改动，例如去掉空格、改变缩进、增删分号
-// build:    构造工具的或者外部依赖的改动，例如webpack，npm
 // refactor: 代码重构时使用
 // revert:   执行git revert打印的message
-
-// # 暂不使用type
+//chore：      不属于以上类型的其他类型
 // test:     添加测试或者修改现有测试
-// perf:     提高性能的改动
-// ci:       与CI（持续集成服务）有关的改动
-// chore:    不修改src或者test的其余修改，例如构建过程或辅助工具的变动
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
