072ec1e40bf0fd575a3a22245d7858d1824f73cd
quanyj <qyjandroid@163.com>
2021-10-14 11:31:58 +0800

    fix: 修复top,right等不能转换rem问题

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,14 @@
 module.exports = {
-    plugins: [
-        require('postcss-flexbugs-fixes'),
-        require("postcss-import"),
-        require("postcss-preset-env"),
-        require("postcss-pxtorem")(
-            {
-                rootValue: 100,
-                unitPrecision : 5 ,
-                minPixelValue: 2,  // 设置要替换的最小像素值
-                propWhiteList: ["font-size", "height", "width", "line-height", "max-wdith","letter-spacing", "max-height",'*position*'],  // Enables converting of all properties – default is just font sizes.
-                selectorBlackList: [".ig-"]  // 忽略的选择器   .ig-  表示 .ig- 开头的都不会转换
-            }
-        )
-    ]
+  plugins: [
+    require('postcss-flexbugs-fixes'),
+    require('postcss-import'),
+    require('postcss-preset-env'),
+    require('postcss-pxtorem')({
+      rootValue: 100,
+      unitPrecision: 5,
+      minPixelValue: 2, // 设置要替换的最小像素值
+      propWhiteList: ['*'], // Enables converting of all properties – default is just font sizes.
+      selectorBlackList: ['.ig-'], // 忽略的选择器   .ig-  表示 .ig- 开头的都不会转换
+    }),
+  ],
 };
-
-
